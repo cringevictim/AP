c89240a35960b647218696743ef375d4430e59f0
cringevictim <cringevictim@gmail.com>
2022-05-03 19:55:31 +0300

    lab11

--- 11/main.c
+++ 11/main.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
-#include <string.h>
-#include <malloc.h>
+
+//Input:
+//200
+//aaee eeaa aaaa eeee bbbb bbee aabb bbaa eebb abobuse ae ab be eb ba aa ee aa
+//Expected result: 3
 
 int main()
 {
-    const char *a = "asdsad";
-    printf("%c",a);
-//    size_t size;
-//    printf("Enter the size of array:\n");
-//    scanf("%d", &size);
-//
-//    char buffer[size + 1]; // <- extra symbol for \0 that is not added automatically
-//    printf("Enter words:\n");
-//    fgets(buffer, sizeof(buffer), stdin);
-//    buffer[size] = '\0';
-//    strcat(buffer, " ");
+    int len;
+    printf("Enter the maximum size of string: ");
+    scanf("%d", &len);
+    len=len+1;
+
+    char str[len];
+    for(int i = 0; i < len; i++)
+    {
+        str[i] = ' ';
+    }
+
+    char c;
+    printf("Enter the string: ");
+    scanf("%c", &c);
+    fgets(str, len, stdin);
+    printf("String: %s", str);
+
+    int check_a, check_e, counter = 0;
+    for(int i = 0; i<len; i++)
+    {
+        if(str[i] == 32 || str[i] == 0 || str[i] == 10)
+        {
+            check_a = 0, check_e = 0;
+        }
+        if(str[i] == 'a')
+        {
+            if(i == 0 || str[i-1] == 32)
+            {
+                check_a = 1;
+            }
+        }
+        if(str[i] == 'e')
+        {
+            if(i == len || str[i+1] == 32 || str[i+1] == 10)
+            {
+                check_e = 1;
+            }
+        }
+        if(check_a == 1 && check_e == 1)
+        {
+            counter = counter + 1;
+        }
+        //printf("\nCycle: %d, check_a: %d, check_e: %d, char: %c, char_int: %d", i, check_a, check_e, str[i], str[i]);
+    }
+    printf("\nAmount: %d", counter);
+    return 0;
 }
